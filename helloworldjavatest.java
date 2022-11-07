0b00854236ab3516d3a15d74c2713ce3926d1ef5
amankaushik1988 <86546191+amankaushik1988@users.noreply.github.com>
2022-11-07 08:14:55 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World - polling test by Aman for Jenkins");
+		System.out.println("Hello World - polling test by Aman for Jenkins on 7 Nov 2022");
 	}
 }
